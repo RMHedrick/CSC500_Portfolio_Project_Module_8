5d0a2b6364a2287d2d52a67bf4ea35bc57b6949b
RMHedrick <rmhcatalyst@gmail.com>
2025-12-21 13:59:52 -0600

    shopping cart - modify item method

--- Shopping Cart.py
+++ Shopping Cart.py
@@ -24,10 +24,32 @@ class ShoppingCart:
 
 
     def remove_item(self, item_name):
-        self.cart_items.remove(item_name)
+        for item in self.cart_items:
+            if item.item_name == item_name:
+                self.cart_items.remove(item)
+                return
+        else:
+            print("Item not found in cart. Nothing Removed.")
 
 
-    def modify_item(self):
+    def modify_item(self, item_to_modify):
+        for item in self.cart_items:
+            if item.item_name == item_to_modify.item_name:
+
+                if item_to_modify.item_name != "none":
+                    item.item_name = item_to_modify.item_name
+
+                if item_to_modify.item_price != 0.0:
+                    item.item_price = item_to_modify.item_price
+
+                if item_to_modify.item_quantity != 0:
+                    item.item_quantity = item_to_modify.item_quantity
+
+                return
+        else:
+            print("Item not found in cart. Nothing Modified.")
+
+
 
     def get_num_items_in_cart(self):
 
