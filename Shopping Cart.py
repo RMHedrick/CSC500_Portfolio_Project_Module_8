4d8ed808601ecc9c232e2f952bfbeeae971b2c98
RMHedrick <rmhcatalyst@gmail.com>
2025-12-21 15:38:44 -0600

    shopping cart - working on menu

--- Shopping Cart.py
+++ Shopping Cart.py
@@ -2,17 +2,16 @@
 
 ## FIX ME will not be able to update properly - define in the init
 class ItemToPurchase:
-    def __init__(self, total_cost = 0.0, item_name = "none", item_description = "none", item_price = 0.0, item_quantity = 0):
-        self.total_cost = total_cost
+    def __init__(self, item_name = "none", item_price = 0.0, item_quantity = 0, item_description = "none"):
         self.item_name = item_name
+        self.item_price = float(item_price)
+        self.item_quantity = int(item_quantity)
         self.item_description = item_description
-        self.item_price = item_price
-        self.item_quantity = item_quantity
+        #self.total_cost = 0.0 #not needed in larger program
 
-#From previous project.
-  #  def print_item_cost(self):
-  #      self.total_cost = self.item_price * self.item_quantity
-  #      print("{} {} @ ${:.2f} = ${:.2f}".format(self.item_name, self.item_quantity, self.item_price, self.total_cost))
+#  def print_item_cost(self):  #not needed in larger program
+#      self.total_cost = self.item_price * self.item_quantity
+#     print("{} {} @ ${:.2f} = ${:.2f}".format(self.item_name, self.item_quantity, self.item_price, self.total_cost))
 
 #Shopping cart class - Add, remove, modify, quantity, cost, total, unique objects, full description
 class ShoppingCart:
@@ -38,14 +37,14 @@ class ShoppingCart:
         for item in self.cart_items:
             if item.item_name == item_to_modify.item_name:
 
-                if item_to_modify.item_name != "none":
-                    item.item_name = item_to_modify.item_name
-
                 if item_to_modify.item_price != 0.0:
                     item.item_price = item_to_modify.item_price
 
                 if item_to_modify.item_quantity != 0:
                     item.item_quantity = item_to_modify.item_quantity
+
+                if item_to_modify.item_description != "none":
+                    item.item_description = item_to_modify.item_description
 
                 return
         else:
@@ -67,7 +66,8 @@ class ShoppingCart:
         return total_cost
 
     def print_total(self):
-        print("{self.customer_name}'s Shopping Cart - {self.current_date}")
+        print(f"{self.customer_name}'s Shopping Cart - {self.current_date}")
+        print()
 
         if len(self.cart_items) == 0:
             print("Shopping Cart Is Empty")
@@ -77,12 +77,39 @@ class ShoppingCart:
             for item in self.cart_items:
                 total_cost = item.item_quantity * item.item_price
                 print("{} {} @ ${:.2f} = ${:.2f}".format(item.item_name, item.item_quantity, item.item_price, total_cost))
-            print("Total: ${:.2f}".format(self.get_cost_of_cart))
+            print("Total: ${:.2f}".format(self.get_cost_of_cart()))
 
 
     def print_descriptions(self):
-        print("{self.customer_name}'s Shopping Cart - {self.current_date}")
+        print(f"{self.customer_name}'s Shopping Cart - {self.current_date}")
         print("Item Descriptions")
         for item in self.cart_items:
-            item.
+            print("{}: {}".format(item.item_name, item.item_description))
+
+def print_menu(cart):
+    print("Menu")
+    print("a - Add item to cart")
+    print("r - Remove item from cart")
+    print("c - Change item quantity")
+    print("i - Output items' descriptions")
+    print("o - Output shopping cart")
+    print("q - Quit")
+
+    user_input = ""
+
+    while user_input != "q":
+        user_input = input("Choose an option: ").lower()
+
+        if user_input == "a":
+            print("Add item to cart")
+            name = str(input("Enter item name: "))
+            description = str(input("Enter item description: "))
+            price = float(input("Enter item price: "))
+            quantity = int(input("Enter item quantity: "))
+            cart.add_item(ItemToPurchase(item_name = name, item_price = price, item_quantity = quantity, item_description = description))
+
+
+
+
+
 
